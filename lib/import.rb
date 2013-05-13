0c4e86a4602309506e38c5ae0429b1dc9a58315b
Daemian Mack <daemianmack@gmail.com>
2013-05-13 12:53:24 -0400

    Move import.rb inside lib/

--- lib/import.rb
+++ lib/import.rb
@@ -1,10 +1,11 @@
+# The original middleman index page.
 rows = File.open("app/views/characters/index.html.haml").read().scan(/td(.*)/)
 
 rows.each_slice(3) do |((text), (series_1), (series_2))|
   element = text.scan(/\.icon-([a-z]*) /).first.first
   name    = text.scan(/\.icon-[a-z]* (.*)/).first.first
 
-  series_1 = series_1.length == 2
+  series_1 = series_1.length == 2 # Populated TD will contain space and checkmark.
   series_2 = series_2.length == 2
 
   c = Character.new(:name => name, :series_1 => series_1, :series_2 => series_2)
