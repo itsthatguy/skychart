860b446dc589ce0d8e383e221196723ed600d174
Daemian Mack <daemianmack@gmail.com>
2013-05-13 12:47:17 -0400

    Blank canvas, new rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skychart::Application.config.secret_token = 'fdfefb9345962947b0fe1a44d290837f70e2057993c3d133960b2e424165e3fc302b6bf047b2011126c7607b0573ed548ca3c8f450bc1c2c8e8880977fc106c1'
