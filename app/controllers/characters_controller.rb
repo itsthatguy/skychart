5508f6f7fb80f0e7600acd7a8a84715ef2feb68e
Daemian Mack <daemianmack@gmail.com>
2013-05-13 12:52:56 -0400

    Add crude support for creating new characters.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,4 +4,20 @@ class CharactersController < ApplicationController
     @characters = Character.all
   end
 
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new(params[:character])
+    respond_to do |format|
+      if @character.save
+        format.html { redirect_to("#index", :notice => "Character successfully created!")}
+      else
+        flash[:notice] = "Error!"
+        format.html { render :action => :new }
+      end
+    end
+  end
+
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,7 @@
 class Character < ActiveRecord::Base
-  attr_accessible :name, :series_1, :series_2
+  attr_accessible :name, :series_1, :series_2, :element_id
   belongs_to :element
+
+  validates :name, :presence => true
+  validates_presence_of :element
 end
